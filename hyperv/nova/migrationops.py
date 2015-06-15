acb783e3ba99b12cee3f5b40dbcd8b715bf8e404
Claudiu Belu <cbelu@cloudbasesolutions.com>
2015-06-15 17:48:35 +0300

    Fixes unstopped console log worked after live migration
    After a live migration is performed, the console.log file
    continues to be open at the source host, as the handler was
    never stopped.
    
    Stops console log handler for the instance before live migration
    is performed.
    
    Change-Id: I866f2159a06497b30b1b8d6d28d699bf0824940a

--- hyperv/nova/migrationops.py
+++ hyperv/nova/migrationops.py
@@ -93,7 +93,8 @@ class MigrationOps(object):
             if dest_path and self._pathutils.exists(dest_path):
                 self._pathutils.rmtree(dest_path)
             if self._pathutils.exists(revert_path):
-                self._pathutils.rename(revert_path, instance_path)
+                self._pathutils.move_folder_contents(revert_path,
+                                                     instance_path)
         except Exception as ex:
             # Log and ignore this exception
             LOG.exception(ex)

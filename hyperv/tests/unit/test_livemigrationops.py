dc6d40e6096590ced753672fcead54e515064a75
Claudiu Belu <cbelu@cloudbasesolutions.com>
2015-06-16 18:18:49 -0700

    Fixes minor unit tests inconsistentcies
    Minor inconsistentcies were introduced. Fixes them.
    
    Conflicts:
    	hyperv/tests/unit/test_livemigrationops.py
    
    (cherry picked from commit 9ae34b98b30d43f956aba953d1ef45a8df342fd9)
    Change-Id: I64388941a728d0b5661a91f10664465fc0f78a87

--- hyperv/tests/unit/test_livemigrationops.py
+++ hyperv/tests/unit/test_livemigrationops.py
@@ -34,10 +34,11 @@ class LiveMigrationOpsTestCase(test_base.HyperVBaseTestCase):
         self._livemigrops._livemigrutils = mock.MagicMock()
         self._livemigrops._pathutils = mock.MagicMock()
 
-    @mock.patch('hyperv.nova.pathutils.PathUtils.copy_vm_console_logs')
+    @mock.patch('hyperv.nova.serialconsoleops.SerialConsoleOps.'
+                'stop_console_handler')
     @mock.patch('hyperv.nova.vmops.VMOps.copy_vm_dvd_disks')
     def _test_live_migration(self, mock_get_vm_dvd_paths,
-                             mock_copy_logs, side_effect):
+                             mock_stop_console_handler, side_effect):
         mock_instance = fake_instance.fake_instance_obj(self.context)
         mock_post = mock.MagicMock()
         mock_recover = mock.MagicMock()
@@ -58,6 +59,9 @@ class LiveMigrationOpsTestCase(test_base.HyperVBaseTestCase):
                                              post_method=mock_post,
                                              recover_method=mock_recover)
 
+            mock_stop_console_handler.assert_called_once_with(
+                mock_instance.name)
+            mock_copy_logs = self._livemigrops._pathutils.copy_vm_console_logs
             mock_copy_logs.assert_called_once_with(mock_instance.name,
                                                    fake_dest)
             mock_live_migr = self._livemigrops._livemigrutils.live_migrate_vm

3a2b460c1e552812c8e7694954c12dfe3b9e3a97
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2015-06-19 13:21:57 +0300

    Reattach the original configdrive during unrescue
    At the moment, the instance original configdrive is not reattached
    when the instance is unrescued. This behavior can be undesired.
    
    This patch fixes the issue by reattaching the instance configdrive
    during the unrescue operation, in case it exists.
    
    Change-Id: I82e2b31139d947a4201c770db37d84b83bba282a

--- hyperv/nova/vmops.py
+++ hyperv/nova/vmops.py
@@ -779,6 +779,12 @@ class VMOps(object):
                            self._ROOT_DISK_CTRL_ADDR, controller_type)
         self._detach_config_drive(instance.name, rescue=True, delete=True)
 
+        # Reattach the configdrive, if exists.
+        configdrive_path = self._pathutils.lookup_configdrive_path(
+            instance.name)
+        if configdrive_path:
+            self.attach_config_drive(instance, configdrive_path, vm_gen)
+
         self.power_on(instance)
 
     def _check_hotplug_is_available(self, instance):

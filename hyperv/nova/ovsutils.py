44be3ab0cfa4ed1361c37b07efe1b94ee1df1346
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2016-07-25 20:05:28 +0300

    Remove duplicated config option
    This patch removes the 'ovs_vsctl_timeout' as it is already defined
    in Nova.
    
    This is currently breaking the CI.
    
    Change-Id: I84d500534cd684207709d33b999743be63a8e117

--- hyperv/nova/ovsutils.py
+++ hyperv/nova/ovsutils.py
@@ -31,14 +31,9 @@ from oslo_log import log as logging
 from hyperv.i18n import _LE
 
 LOG = logging.getLogger(__name__)
-net_cfg = [
-    cfg.IntOpt('ovs_vsctl_timeout',
-               default=120,
-               help='Amount of time, in seconds, that ovs_vsctl should wait '
-                    'for a response from the database. 0 is to wait forever.'),
-]
+
 CONF = cfg.CONF
-CONF.register_opts(net_cfg)
+CONF.import_opt('ovs_vsctl_timeout', 'nova.conf.network')
 
 
 def _ovs_vsctl(args):
